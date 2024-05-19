89fe66f16d1eaf3c9146709591dda29c31d5584b
Anzar Malik <anzarmalikcse@gmail.com>
2024-05-19 22:22:46 +0530

    minor change added

--- nest-js-auth/src/auth-dev-testing/auth-dev-testing.controller.ts
+++ nest-js-auth/src/auth-dev-testing/auth-dev-testing.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Req, UseGuards } from '@nestjs/common';
 import { AuthDevTestingService } from './auth-dev-testing.service';
 import { AuthGuard } from '@nestjs/passport';
 
+/* This controller is just to check wether jwt token is working as expected!*/
 @Controller('auth-dev-testing')
 export class AuthDevTestingController {
   constructor(private readonly authDevTestingService: AuthDevTestingService) {}
--- nest-js-auth/src/auth-dev-testing/auth-dev-testing.module.ts
+++ nest-js-auth/src/auth-dev-testing/auth-dev-testing.module.ts
@@ -3,6 +3,10 @@ import { AuthDevTestingController } from './auth-dev-testing.controller';
 import { AuthDevTestingService } from './auth-dev-testing.service';
 import { AuthModule } from 'src/auth/auth.module';
 
+/* This controller is just to check wether jwt token is working as expected!
+To ensure future restricted api's will work as expected
+*/
+
 @Module({
   imports: [AuthModule],
   controllers: [AuthDevTestingController],
